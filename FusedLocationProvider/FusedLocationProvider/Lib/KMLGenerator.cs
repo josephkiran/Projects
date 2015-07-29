3229ed3f6f8f5463980f33b79d2a186fd2094209
josephkiran <josephkiran@gmail.com>
2015-07-29 22:50:29 +0530

    ploted kml in map

--- FusedLocationProvider/FusedLocationProvider/Lib/KMLGenerator.cs
+++ FusedLocationProvider/FusedLocationProvider/Lib/KMLGenerator.cs
@@ -19,6 +19,40 @@ namespace FusedLocationProvider.Lib
         public static Document GenerateKML(List<Segment> segments)
         {
             Document d = new Document();
+            d.Style = new List<Style>();
+            Style st = new Style();
+            st.Id = "yellowLine";
+            st.LineStyle = new LineStyle();
+            st.LineStyle.Color = "7f00ffff";
+            st.LineStyle.Width = "4";
+            d.Style.Add(st);
+            st = new Style();
+            st.Id = "greenLine";
+            st.LineStyle = new LineStyle();
+            st.LineStyle.Color = "7d00ff00";
+            st.LineStyle.Width = "4";
+            d.Style.Add(st);
+            st = new Style();
+            st.Id = "redLine";
+            st.LineStyle = new LineStyle();
+            st.LineStyle.Color = "ff0000ff";
+            st.LineStyle.Width = "4";
+            d.Style.Add(st);
+            st = new Style();
+            st.Id = "blueLine";
+            st.LineStyle = new LineStyle();
+            st.LineStyle.Color = "ffff0000";
+            st.LineStyle.Width = "4";
+            d.Style.Add(st);
+            st = new Style();
+            st.Id = "blackLine";
+            st.LineStyle = new LineStyle();
+            st.LineStyle.Color = "87000000";
+            st.LineStyle.Width = "4";
+            d.Style.Add(st);
+
+
+
             d.Placemarks = new List<Placemark>();
 
             for (int i = 0; i < segments.Count; i++)
@@ -27,11 +61,40 @@ namespace FusedLocationProvider.Lib
                 Placemark p = new Placemark();
                 p.LineString = new LineString();
                 p.LineString.AltitudeMode = "absolute";
-                p.LineString.Coordinates = string.Format("{0},{1},0,{2},{3},0", item.StartLat, item.StartLog, item.EndLat, item.EndLog);
+                p.LineString.Coordinates = string.Format("{0},{1},0,{2},{3},0", item.StartLog,item.StartLat , item.EndLog, item.EndLat );
+                p.StyleUrl = GetRoadConditionStyleID(item.RoadCondition);
                 d.Placemarks.Add(p);
             }
 
             return d;
+        }
+
+        public static string GetRoadConditionStyleID(RoadType rdtype)
+        {
+            string styleID = "#greenLine";
+
+            switch (rdtype)
+            {
+                case RoadType.Good:
+                case RoadType.Idle:
+                case RoadType.RandomAction:
+                    styleID = "#greenLine";
+                    break;
+                   
+                case RoadType.SlightyBumpy:
+                    styleID = "#yellowLine";
+                    break;
+                case RoadType.Bumpy:
+                    styleID = "#blueLine";
+                    break;
+                case RoadType.Worst:
+                    styleID = "#redLine";
+                    break;
+                default:
+                    styleID = "#blackLine";
+                    break;
+            }
+            return styleID;
         }
 
         
